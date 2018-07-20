3aae2015fa99dac1fd9f35dd7d69c5c3fc46b986
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-20 02:39:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,5 +66,15 @@ class Dog
     doggy
   end
 
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, id).map do |roq|
+      self.new_from_db(row)
+    end.first
+  end
+
+
 
 end
